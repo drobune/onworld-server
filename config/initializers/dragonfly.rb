4410b3ce73b458439355f769b1d974e9e257349e
yu1ch1 <s.yuichi227@gmail.com>
2014-04-06 06:03:59 +0900

    initial comiit;

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  protect_from_dos_attacks true
+
+  secret '823fd8f11037a37378d6e2a1abbb90795ba49d0b5879c5f4fb5ad195e55aa338'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnworldRec::Application.config.secret_token = '3e7ce4185aeb6c89ec83f2d4f1de7181c68e5b7ff85870d1e0a7f4bf471b4864c86a599e68da7a0e8df326d10357033e946fcf1be7d942ccd7508ea8ea7913be'
